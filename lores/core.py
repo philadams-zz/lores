b972460f3ac505e5f8f45f061915a105714bda79
Phil Adams <phil@philadams.net>
2013-02-23 10:31:52 -0500

    working alpha proof of concept

--- lores/core.py
+++ lores/core.py
@@ -7,26 +7,33 @@ Phil Adams http://philadams.net
 Render images on the terminal.
 """
 
+import sys
+from StringIO import StringIO
+
+from PIL import Image
+import requests
+import vari
+
+
+def puts(clr):
+    c = vari.Vari(rgb=clr)
+    fmt = '\x1b[48;5;%sm  \x1b[0m'
+    return fmt % c.x256
+
 def main(args):
 
-    # parse csv data
-    rows = csv.reader(args.infile,
-            delimiter=args.delimiter, quotechar=args.quotechar)
-
-    # skip n rows
-    for i in range(args.skip):
-        rows.next()
-
-    # prep fields
-    if not args.fields:
-        fields = ''
-    fields = set([int(f) for f in args.fields.replace(' ', '').split(',')])
-
-    # push to stdout
-    out = csv.writer(sys.stdout)
-    for row in pluck(rows, fields):
-        out.writerow(row)
-
+    #req = requests.get(args.imfile)
+    #im = Image.open(StringIO(req.content))
+    im = Image.open(args.imfile)
+    maxw = 80
+    im.thumbnail((40, 40), Image.ANTIALIAS)
+    w, h = im.size
+    pxs = im.load()
+    for y in range(h):
+        for x in range(w):
+            px = pxs[x, y]
+            sys.stdout.write(puts(px))
+        sys.stdout.write('\n')
 
 def cli():
     import argparse
